0347cd2e541bdbaecc052fe8381e14250af6abbd
Julian Nodorp <jnodorp@jaconi.io>
2025-03-28 14:20:08 +0100

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.jaconi"
@@ -19,7 +19,7 @@ repositories {
 
 dependencyManagement {
 	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.1")
 	}
 }
 
